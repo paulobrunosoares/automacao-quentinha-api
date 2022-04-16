9e5cac9e5c1fa82fdfa42d8c6a80952888399b50
PauloBSS <pbss2007@gmail.com>
2022-04-15 22:26:57 -0300

    apiRest-jwt-auth-service

---
+++ src/main/java/quentinha/dto/JwtTokenDTO.java
@@ -0,0 +1,28 @@
+package quentinha.dto;
+
+public class JwtTokenDTO {
+
+	public String token;
+
+	public JwtTokenDTO() {
+	}
+
+	public JwtTokenDTO(String token) {
+		this.token = token;
+	}
+
+	public String getToken() {
+		return this.token;
+	}
+	
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	@Override
+	public String toString() {
+		return "JwtTokenDTO [token=" + token + "]";
+	}
+	
+	
+}
---
+++ src/main/java/quentinha/dto/UserDataDTO.java
@@ -0,0 +1,23 @@
+package quentinha.dto;
+
+import java.util.List;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import quentinha.model.AppUserRole;
+
+@Data
+@NoArgsConstructor
+public class UserDataDTO {
+  
+  @ApiModelProperty(position = 0)
+  private String username;
+  @ApiModelProperty(position = 1)
+  private String email;
+  @ApiModelProperty(position = 2)
+  private String password;
+  @ApiModelProperty(position = 3)
+  List<AppUserRole> appUserRoles;
+
+}
